7ba2bc946f3541a3a0e901fc7f2754d17782be36
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-30 23:03:06 +0200

    weight init is added in the utils file

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import yaml
 import joblib
 import torch
+import torch.nn as nn
 
 
 def params():
@@ -35,3 +36,14 @@ def device_init(device="mps"):
 
     else:
         return torch.device("cuda")
+
+
+def weight_init(m):
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
